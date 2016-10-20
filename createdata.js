973a60db628f8c512bf66aa35abe7f4ce0250517
Christopher Harrison <chrhar@microsoft.com>
2016-10-20 16:07:36 -0700

    Updated to support new object model

--- createdata.js
+++ createdata.js
@@ -1,58 +1,64 @@
-var SalesPerson = require('./models/salesperson.js');
-var mongoose = require('mongoose');
+const Salesperson = require('./models/salesperson.js');
+const Invoice = require('./models/invoice.js');
+const mongoose = require('mongoose');
 
 createData();
 
 function createData() {
     mongoose.connect('mongodb://localhost/invoicing');
 
-    SalesPerson.remove({}).exec(function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            var salesPeople = [];
-            ['Peter', 'Kelly', 'Brandy', 'Faye', 'Marcus', 'Heriberto', 'Marquita', 'Bethany', 'Larry', 'Christopher'].forEach(function(name) {
-                var salesPerson = {};
-                salesPerson.name = name;
-                salesPerson.email = name + '@fourthcoffee.com';
-                var invoiceCount = Math.floor(Math.random() * 20) + 1;
-                salesPerson.invoices = []
-                for(var index = 0; index < invoiceCount; index++) {
-                    var invoice = {
-                        company: getItem(['AdventureWorks', 'Contoso', 'Blue Yonder Airlines', 'Coho Winery', 'Northwind Traders', 'Wingtip Toys']),
-                        state: getItem(['Open', 'Closed', 'Past Due']),
-                        items: getLineItems()
-                    };
-                    salesPerson.invoices.unshift(invoice);                   
-                }
-                salesPeople.unshift(salesPerson);
-            });
-
-            SalesPerson.create(salesPeople, function(err) {
-                if(err) console.log(err);
-                else console.log('SalesPeople Created!!');
-            });
-        }
-    });
+    Salesperson.remove({})
+        .exec((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                Invoice.remove({})
+                    .exec((err) => {
+                        ['Peter', 'Kelly', 'Brandy', 'Faye', 'Marcus', 'Heriberto', 'Marquita', 'Bethany', 'Larry', 'Christopher'].forEach(function (name) {
+                            var newSalesperson = {
+                                name: name,
+                                email: name + '@fourthcoffee.com'
+                            };
+                            Salesperson.create(newSalesperson, (err, salesperson) => {
+                                if (err) console.log(err);
+                                else {
+                                    const invoices = [];
+                                    var invoiceCount = Math.floor(Math.random() * 20) + 1;
+                                    for (var index = 0; index < invoiceCount; index++) {
+                                        var invoice = {
+                                            company: getItem(['AdventureWorks', 'Contoso', 'Blue Yonder Airlines', 'Coho Winery', 'Northwind Traders', 'Wingtip Toys']),
+                                            state: getItem(['Open', 'Closed', 'Past Due']),
+                                            items: getLineItems(),
+                                            _salespersonId: salesperson._id
+                                        };
+                                        invoices.push(invoice);
+                                    }
+                                    Invoice.create(invoices);
+                                }
+                            });
+                        });
+                    });
+            }
+        });
 }
 
 function getLineItems() {
     var products = [
-        {name: 'Filters', price: 10},
-        {name: 'Coffee Machine', price: 200},
-        {name: 'Coffee Pot', price: 50},
-        {name: 'Mug', price: 5},
-        {name: 'Assorted Teas', price: 20},
-        {name: 'Flavored Creamer', price: 5},
-        {name: 'Sugar', price: 6},
-        {name: 'Thermos', price: 30},
-        {name: 'Stirrers', price: 4},
-        {name: 'Coffee Beans', price: 8}
+        { name: 'Filters', price: 10 },
+        { name: 'Coffee Machine', price: 200 },
+        { name: 'Coffee Pot', price: 50 },
+        { name: 'Mug', price: 5 },
+        { name: 'Assorted Teas', price: 20 },
+        { name: 'Flavored Creamer', price: 5 },
+        { name: 'Sugar', price: 6 },
+        { name: 'Thermos', price: 30 },
+        { name: 'Stirrers', price: 4 },
+        { name: 'Coffee Beans', price: 8 }
     ]
     var number = Math.floor(Math.random() * (products.length - 1)) + 1;
 
     var items = [];
-    for(var counter = 0; counter < number; counter++) {
+    for (var counter = 0; counter < number; counter++) {
         var index = Math.floor(Math.random() * (products.length - 1));
         var item = products.splice(index, 1)[0];
         item.number = Math.floor(Math.random() * 10) + 1;
@@ -63,5 +69,5 @@ function getLineItems() {
 }
 
 function getItem(items) {
-    return items[(Math.floor( Math.random() * (items.length - 1) ))];
+    return items[(Math.floor(Math.random() * (items.length - 1)))];
 }

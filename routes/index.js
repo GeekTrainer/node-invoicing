5518a51df12b1b558962b779222948a8d8613fb1
Christopher Harrison <chrhar@microsoft.com>
2016-10-20 13:45:23 -0700

    Updated to use ES6

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Salesperson = require('../models/salesperson.js');
 
-router.get('/', function(req, res, next) {
-  Salesperson.find().select('name _id').sort('name').exec(function(err, salesPeople) {
+router.get('/', (req, res, next) => {
+  Salesperson.find().select('name _id').sort('name').exec((err, salesPeople) => {
     if(!err) {
       console.log(salesPeople);
       res.render('index', {salesPeople: salesPeople});

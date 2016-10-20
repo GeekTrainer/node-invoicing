6391a7dd741f3aa7c58edc1fdd349733390240cd
Christopher Harrison <chrhar@microsoft.com>
2016-10-20 13:50:26 -0700

    renamed variable

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var Salesperson = require('../models/salesperson.js');
 
 router.get('/', (req, res, next) => {
-  Salesperson.find().select('name _id').sort('name').exec((err, salesPeople) => {
+  Salesperson.find().select('name _id').sort('name').exec((err, salespeople) => {
     if(!err) {
-      console.log(salesPeople);
-      res.render('index', {salesPeople: salesPeople});
+      console.log(salespeople);
+      res.render('index', {salespeople: salespeople});
     } else {
       console.log(err);
     }

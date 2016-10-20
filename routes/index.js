f5de21dd8a37c8403c7a9abcc9ccdbc766c763ef
Christopher Harrison <chrhar@microsoft.com>
2016-10-20 13:44:57 -0700

    Fixed variable name

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var SalesPerson = require('../models/salesperson.js');
+var Salesperson = require('../models/salesperson.js');
 
 router.get('/', function(req, res, next) {
-  SalesPerson.find().select('name _id').sort('name').exec(function(err, salesPeople) {
+  Salesperson.find().select('name _id').sort('name').exec(function(err, salesPeople) {
     if(!err) {
       console.log(salesPeople);
       res.render('index', {salesPeople: salesPeople});
